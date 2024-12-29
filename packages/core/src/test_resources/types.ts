13a89d67374526bdd3b9f38e386b351ad6c96570
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 21:37:03 -0500

    docs: Add JSDoc documentation to types.ts

--- packages/core/src/test_resources/types.ts
+++ packages/core/src/test_resources/types.ts
@@ -1,3 +1,10 @@
+/**
+ * Interface representing a User
+ * @property {string} id - The unique identifier of the user
+ * @property {string} [email] - The email address of the user (optional)
+ * @property {string} [phone] - The phone number of the user (optional)
+ * @property {string} [role] - The role of the user (optional)
+ */
 export interface User {
     id: string;
     email?: string;
