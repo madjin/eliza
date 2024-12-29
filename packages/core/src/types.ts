f213d482675385ead845e13ec8ba6dec7ca12a09
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 21:37:24 -0500

    docs: Add JSDoc documentation to types.ts

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -3,6 +3,9 @@ import { Readable } from "stream";
 /**
  * Represents a UUID string in the format "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
  */
+/**
+ * Represents a universally unique identifier (UUID).
+ */
 export type UUID = `${string}-${string}-${string}-${string}-${string}`;
 
 /**
